10deac22e8aff3b1f152a62f4330bdbb74e18647
michaelcoitim <michaelcoitim1@gmail.com>
2022-07-27 10:21:36 -0300

    criando classe agenda

--- src/com/agenda/model/Agenda.java
+++ src/com/agenda/model/Agenda.java
@@ -1,9 +1,42 @@
 package com.agenda.model;
 
-public class Agenda {
+import java.util.Date;
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("oi");
+public class Agenda {
+    private int id;
+    private String nome;
+    private int idade;
+    private Date dataCadastro;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
     }
 }
