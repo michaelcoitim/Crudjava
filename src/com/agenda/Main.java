1d69f350f84c9734484efbcbde429acc61b30cb5
michaelcoitim <michaelcoitim1@gmail.com>
2022-07-27 09:19:37 -0300

    criando projeto/ importando os drivers de conexão

--- src/com/agenda/Main.java
+++
@@ -1,8 +0,0 @@
-package com.agenda;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/com/agenda/model/Agenda.java
@@ -0,0 +1,9 @@
+package com.agenda.model;
+
+public class Agenda {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("oi");
+    }
+}
