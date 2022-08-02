759430950271f99b3237c8454950db5b61dd51bb
michaelcoitim <michaelcoitim1@gmail.com>
2022-08-02 15:53:56 -0300

    Criando classe DAO metodo SAVE

---
+++ src/com/agenda/contato/dao/ContatoDAO.java
@@ -0,0 +1,17 @@
+package com.agenda.contato.dao;
+
+import com.agenda.model.Agenda;
+
+/*
+* O dao é basicamente onde fica as regra s de negocio
+* onde o crud é de fato feito
+* onde moldamos o java com o banco relacional
+* DAO é um acrônimo do inglês Data Access Object - DAO
+* ou seja Objeto de acesso a dados */
+public class ContatoDAO {
+
+    // metodo save é o create
+    public void save(Agenda contato){
+
+    }
+}
