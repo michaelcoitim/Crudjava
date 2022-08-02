127fc2ff869cc3f8516e5b25bdf43d03d6eed669
michaelcoitim <michaelcoitim1@gmail.com>
2022-08-02 16:27:47 -0300

    Metodo SAVE finalizando

--- src/com/agenda/contato/dao/ContatoDAO.java
+++ src/com/agenda/contato/dao/ContatoDAO.java
@@ -1,6 +1,11 @@
 package com.agenda.contato.dao;
 
+import com.agenda.factory.ConnectionFactory;
 import com.agenda.model.Agenda;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 
 /*
 * O dao é basicamente onde fica as regra s de negocio
@@ -11,9 +16,46 @@ import com.agenda.model.Agenda;
 public class ContatoDAO {
 
     // metodo save é o create
-    public void save(Agenda contato){
+    public void save(Agenda contato) throws Exception {
+        // string de insersão
         String sql = "INSERT INTO contato(nome, idade,dataCadastro) " +
-                      "VALUES(?,?,)";
+                      "VALUES(?, ?, ?)";
+        //crinado conexão banco
+        Connection conn = null;
+        //utilizado para execultar consutas
+        PreparedStatement pstm = null;
+
+        // tentar conectar no banco
+        try {
+            // cria a conexão com banco
+                conn = ConnectionFactory.createConnectionToMysql();
+
+            // crinado uma PreparedStatement para execultar o comando sql
+                pstm = (PreparedStatement)  conn.prepareStatement(sql);
+            //setando os valores para gravar em banco
+                pstm.setString(1, contato.getNome());
+                pstm.setInt(2,contato.getIdade());
+                pstm.setDate(3, new Date(contato.getDataCadastro().getTime()));
+
+            //execultado a query
+                pstm.execute();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            // fechar todas as conexões com o banco
+            try {
+                if (pstm != null){
+                    pstm.close();
+                }
+                if (conn !=null){
+                    conn.close();
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
 }
